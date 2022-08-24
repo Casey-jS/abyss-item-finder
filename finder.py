d096d7c1281a4681ef56c393b13cdbf6539255ec
Casey Sytsema <sytsemacj@gmail.com>
2022-08-24 15:00:57 -0400

    [Adds comments and refactor]

--- finder.py
+++ finder.py
@@ -2,20 +2,9 @@ from bs4 import BeautifulSoup as bs
 import requests as r
 
 class Finder:
-
     def __init__(self, app, desc):
         self.app = app
         self.desc = desc
-    
-    @property 
-    def desc(self):
-        return self.__desc
-    
-    @desc.setter
-    def desc(self, input):
-        self.__desc = input
-    
-
 
     def find(self, item_name: str) -> str:
 
@@ -31,41 +20,15 @@ class Finder:
         # find the table of type wikitable
         table = bs_page.find('table', class_='wikitable mw-collapsible')
 
-        # all items and descriptions
-        items = {}
-
         # for each row in the table
         for row in table.tbody.find_all('tr'):
 
             # all columns in the row
             columns = row.find_all('td')
+
+            # the second column in each row is the item's name
             item = columns[1].text.strip().lower()
             if columns != []:
-                if item_name in item:
-                    return str(item) + ":\n"
-
-            if columns != []:
-                item = columns[1].text.strip().lower()
-                desc = columns[2].text.strip()
-
-                items[item] = desc
-        
-        # Basic search
-        for item in items:
-            if item_name in item:
-                return str(item) + ":\n" + str(items[item])
+                if item_name in item: # the third column in each row is the item's description
+                    return str(item) + ":\n" + str(columns[2].text.strip())
         return "Nothing found"
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
